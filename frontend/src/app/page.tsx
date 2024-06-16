e3067a42963d0e415b25319e3f678a73e03d0f32
Sameer <samirtiwari567@gmail.com>
2024-06-16 12:50:51 +0545

    added backend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,14 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section>
+      <MaxWidthWrapper>
+        <div>
+
+        </div>
+      </MaxWidthWrapper>
+    </section>
+  );
+}
